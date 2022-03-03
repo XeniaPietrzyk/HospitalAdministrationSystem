b60cbc3141bb90e116028fc20accc42bddbc32a1
Xenia Pietrzyk <xeniapietrzyk@gmail.com>
2022-03-03 20:27:01 +0100

    changed names

---
+++ Model/Controllers/DutyController.cs
@@ -0,0 +1,104 @@
+﻿using Model.Model;
+using Model.Service;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Model.Services
+{
+    //NOTE: do poprawki na przyszlosc - osobny interface dla Duty
+    public class DutyController : IEmployeeController<Duty>
+    {
+        //NOTE: wlasciwie mozna by poprawic zwracanie List<Duty> na komunikat o udanej lub nieudanej akcji (?)
+        public List<Duty> Add(Duty entity, Context context)
+        {
+            List<Duty> duties = context.Duties;
+            entity.Id = GenerateId(context);
+            entity.Shifts = new List<Shift>();
+            duties.Add(entity);
+            context.Duties = duties;
+            return duties;
+        }
+
+        public IQueryable<Duty> GetAll(Context context)
+        {
+            return context.Duties.AsQueryable();
+        }
+
+        public Duty FindFirstByCondition(int id, Context context)
+        {
+            Duty duty = context.Duties.FirstOrDefault(x => x.Id == id);
+            return duty;
+        }
+
+        public List<Duty> Update(Duty entity, Context context)
+        {
+            List<Duty> duties = context.Duties;
+            var oldDuty = FindFirstByCondition(entity.Id, context);
+            //szuka indeksu tej zmiany w AllShifts
+            var index = duties.IndexOf(oldDuty);
+            //nadpisuje shift w miejscu oldShift
+            context.Duties[index] = entity;
+            //zwraca zmiany
+            return duties;
+        }
+
+        public void Delete(int id, Context context)
+        {
+            List<Duty> duties = context.Duties;
+            duties.RemoveAt(id);
+        }
+
+        private int GenerateId(Context context)
+        {
+            var id = 1;
+            if (context.Duties.Count != 0)
+            {
+                id = context.Duties.Max(x => x.Id) + 1;
+            }
+            return id;
+        }
+
+        public Shift GetShift(Duty duty, int shiftId, Context context)
+        {
+            var shift = duty.Shifts.FirstOrDefault(x => x.Id == shiftId);
+            return shift;
+        }
+
+        public List<Shift> GetShifts(Duty duty, Context context)
+        {
+            List<Shift> dutyShifts = new List<Shift>();
+            foreach (var item in duty.Shifts)
+            {
+                dutyShifts.Add(item);
+            }
+            return dutyShifts;
+        }
+
+        public Shift AddShift(Duty entity, Shift shift, Context context)
+        {
+            //znajduje dyzur po id z listy Duties
+            var duty = FindFirstByCondition(entity.Id, context);
+            //dodaje nowa zmiane do dyzuru
+            duty.Shifts.Add(shift);
+            //altualizuje dyzur
+            Update(duty, context);
+            return shift;
+        }
+
+        public List<Shift> DeleteShift(int id, Shift shift, Context context)
+        {          
+            //znajdz duty, ktore posiada shift
+            var duty = FindFirstByCondition(id, context);
+            var dutyShifts = duty.Shifts;
+            //znajdz id shifta w duty
+            shift = dutyShifts.FirstOrDefault(x => x.Id == shift.Id);
+            var index = dutyShifts.IndexOf(shift);
+            //usun shift z duty
+            duty.Shifts.RemoveAt(index);
+            //zaktualizuj Duties
+            Update(duty, context);
+            //zwroc aktualna liste duty.shift
+            return duty.Shifts;
+        }
+    }
+}
---
+++ Model/Controllers/NurseController.cs
@@ -0,0 +1,101 @@
+﻿using Model.Helpers;
+using Model.Model;
+using Model.Service;
+using Model.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Model.Controller
+{
+    public class NurseController : Validate, IEmployeeController<Nurse>
+    {
+        public List<Nurse> Add(Nurse entity, Context context)
+        {
+            //pobiera wszystkich pielegniarzy z bazy (plku xml)
+            List<Nurse> nurses = context.Nurses;
+            //nadaje id nowemu pielegniarzowi
+            entity.Id = GenerateId(context);
+            entity.Shift = new List<Shift>();
+            //dodaje piel. do listy piel.
+            nurses.Add(entity);
+            //zwraca piel.
+            return nurses;
+        }
+
+        public void Delete(int id, Context context)
+        {
+            List<Nurse> nurses = context.Nurses;
+            var index = nurses.FindIndex(x => x.Id == id);
+            nurses.RemoveAt(index);
+            context.Nurses = nurses;
+        }
+
+
+        public Nurse FindFirstByCondition(int id, Context context)
+        {
+            Nurse nurse = context.Nurses.FirstOrDefault(x => x.Id == id);
+            return nurse;
+        }
+
+        public IQueryable<Nurse> GetAll(Context context)
+        {
+            return context.Nurses.AsQueryable();
+        }
+
+
+        public List<Nurse> Update(Nurse entity, Context context)
+        {
+            //pobiera z bazy wszystkich piel.
+            List<Nurse> nurses = context.Nurses;
+            //znajduje piel. o entity.id         
+            var oldNurse = FindFirstByCondition(entity.Id, context);
+            //znajduje index tego piel. na liscie wszystkich lekarzy
+            var index = nurses.IndexOf(oldNurse);
+            //nadpisuje: entity w miejscu oldPhysician
+            context.Nurses[index] = entity;
+            //zwraca liste wszystkich physicians
+            return nurses;
+        }
+
+        public Shift AddShift(Nurse entity, Shift shift, Context context)
+        {
+            //przypisuje pielegniarza (skonwertowana do medyka) do zmiany
+            ConvertToMedic converter = new ConvertToMedic(entity);
+            //Medic convertedMedic = converter.ConvertNurse();
+            Medic converted = converter.ConvertNurse();
+            shift.Medic = converted;
+            //dodaje zmiane do listy zmian piel.
+            entity.Shift.Add(shift);
+            //aktualizacja piel.
+            Update(entity, context);
+            //zwraca zmiane
+            return shift;
+        }
+
+        public List<Shift> GetShifts(Nurse entity, Context context)
+        {
+            List<Shift> shifts = new List<Shift>();
+            foreach (var item in entity.Shift)
+            {
+                shifts.Add(item);
+            }
+            return shifts;
+        }
+
+        public List<Shift> DeleteShift(int id, Shift shift, Context context)
+        {
+            var entity = FindFirstByCondition(id, context);
+            var entityShifts = entity.Shift;
+            shift = entity.Shift.FirstOrDefault(x => x.Id == shift.Id);
+            var index = entityShifts.IndexOf(shift);
+            entity.Shift.RemoveAt(index);
+            Update(entity, context);
+            return entity.Shift;
+        }
+
+        public Shift GetShift(Nurse entitty, int shiftId, Context context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
